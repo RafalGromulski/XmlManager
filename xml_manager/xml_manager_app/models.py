9f6385a41c7138e780aa39226f94cc5f25a84ef8
Rafał Gromulski <rgromulski@gmail.com>
2024-02-09 12:42:23 +0100

    2. Data model introduced

--- xml_manager/xml_manager_app/models.py
+++ xml_manager/xml_manager_app/models.py
@@ -1,13 +1,37 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class TslValidityInfo(models.Model):
+class TspServiceDetails(models.Model):
+    class TspServiceStatus(models.TextChoices):
+        GRANTED = "Granted"
+        WITHDRAWN = "Withdrawn"
+
+    class ServiceStatus(models.TextChoices):
+        NEW_NOT_SERVED = "Nie obsłużona (nowa)", _("Nie obsłużona (nowa)")
+        WITHDRAWN_NOT_SERVED = "Nie obsłużona (wycofana)", _("Nie obsłużona (wycofana)")
+        SERVED = "Obsłużona", _("Obsłużona")
+
+    class CrlUrlStatus(models.TextChoices):
+        URL_DEFINED = "CRL URL ustalony", _("CRL URL ustalony")
+        URL_UNDEFINED = "CRL URL nieustalony", _("CRL URL nieustalony")
+
     country_code = models.CharField(verbose_name="Kod państwa", max_length=2)
     country_name = models.CharField(verbose_name="Państwo", max_length=20)
-    tsl_operator_name = models.CharField(verbose_name="Nazwa operatora TSL", max_length=255)
-    tsl_issue_date = models.DateTimeField(verbose_name="Data wydania TSL")
-    tsl_expiry_date = models.DateTimeField(verbose_name="Data ważności TSL")
-    tsl_validity_alert = models.CharField(verbose_name="Status TSL", max_length=100, default="")
+    tsp_name = models.CharField(verbose_name="Dostawca usługi", max_length=255)
+    service_name = models.CharField(verbose_name="Nazwa usługi", max_length=255)
+    service_status = models.CharField(verbose_name="Status usługi", max_length=50, choices=TspServiceStatus.choices)
+    service_start_date = models.DateTimeField(verbose_name="Start usługi")
+    tsp_url = models.CharField(verbose_name="TSP URL", max_length=150, default="")
+    crl_url = models.URLField(verbose_name="CRL URL", max_length=150, default="")
+    crl_url_status_app = models.CharField(verbose_name="Status CRL URL", max_length=50, choices=CrlUrlStatus.choices)
+    service_type = models.CharField(verbose_name="Typ usługi", max_length=100, default="")
+    service_status_app = models.CharField(
+        verbose_name="Status obsługi",
+        max_length=100,
+        choices=ServiceStatus.choices,
+    )
+    service_digital_id = models.TextField(verbose_name="ID usługi", default="")
 
     def __str__(self):
-        return "Country: " + str(self.country_name) + " — TSL Operator Name: " + str(self.tsl_operator_name)
+        return "TSP: " + str(self.tsp_name) + " — Service: " + str(self.service_name)
--- xml_manager/xml_manager_project/settings.py
+++ xml_manager/xml_manager_project/settings.py
@@ -122,3 +122,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "services_to_served"
+LOGOUT_REDIRECT_URL = "main_view"
